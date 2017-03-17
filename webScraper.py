741b5cda78789d6a9be13afeb728998063fa829c
Aniket Pandey <aniketp@iitk.ac.in>
2017-03-17 05:06:22 -0400

    Added webScaper

---
+++ webScraper.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+def trade_spider(max_pages):
+    page = 1
+    while page <= max_pages:
+        url = 'https://github.com/sakshamsharma?page=' + str(page) +'&tab=repositories'
+        source_code = requests.get(url)
+        plain_text = source_code.text
+
+        soup = BeautifulSoup(plain_text,"lxml")
+        for link in soup.findAll('a',{'itemprop':'name codeRepository'}):
+            href = 'https://github.com' + link.get('href')
+            title = link.string
+            get_about_data(href,title)
+            
+        page += 1
+
+
+def get_about_data(repo_url,title):
+    source_code = requests.get(repo_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text,"lxml")
+
+    for about in soup.findAll('span',{'itemprop':'about'}):
+        print(str(title) + ' - ' + str(about.string))        
+            
+trade_spider(3)
+
